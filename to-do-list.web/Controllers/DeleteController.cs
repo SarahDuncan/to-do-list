cab805c3694e2c4dcb1e09a060bd2e0b91215785
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-04-04 20:26:56 +0100

    Add a delete controller with a delete to-do item and a delete to-do list endpoint

---
+++ to-do-list.web/Controllers/DeleteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using to_do_list.web.Data;
+
+namespace to_do_list.web.Controllers
+{
+    public class DeleteController : Controller
+    {
+        private readonly ToDoListDbContext _dbContext;
+
+        public DeleteController(ToDoListDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> DeleteItem(int itemId)
+        {
+            if (itemId == 0)
+            {
+                return NotFound();
+            }
+
+            var todoItem = _dbContext.ToDoItemModel.FirstOrDefault(t => t.Id == itemId);
+            _dbContext.ToDoItemModel.Remove(todoItem);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> DeleteList()
+        {
+            var todoList = _dbContext.ToDoItemModel.ToList();
+            _dbContext.RemoveRange(todoList);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
