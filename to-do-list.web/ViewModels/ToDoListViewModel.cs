362770e4bc63736297aae7567ce91908e6fa3ce1
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-04-04 20:27:51 +0100

    Add view models for data transfer from controllers to views and back

---
+++ to-do-list.web/ViewModels/ToDoListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using to_do_list.web.Models;
+
+namespace to_do_list.web.ViewModels
+{
+    public class ToDoListViewModel
+    {
+        public List<ToDoItemModel> ToDoList { get; set; }
+        public ToDoItemViewModel ToDoItem { get; set; }
+
+    }
+}
