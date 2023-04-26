33111562a8d30a7072bdd63fcebc9cc0b44b4c88
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-04-26 18:56:51 +0100

    resolve merge conflicts

---
+++ to-do-list.web/Controllers/DeleteController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using to_do_list.web.Data;
+
+namespace to_do_list.web.Controllers
+{
+    public class DeleteController : Controller
+    {
+        private readonly ToDoListDbContext _dbContext;
+
+        public DeleteController(ToDoListDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteItem(int itemId)
+        {
+            if (itemId == 0)
+            {
+                return NotFound();
+            }
+
+            var todoItem = _dbContext.ToDoItemModel.FirstOrDefault(t => t.Id == itemId);
+            _dbContext.ToDoItemModel.Remove(todoItem);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteList()
+        {
+            var todoList = _dbContext.ToDoItemModel.ToList();
+            _dbContext.RemoveRange(todoList);
+            await _dbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ to-do-list.web/Controllers/EditController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using to_do_list.web.Data;
+using to_do_list.web.Models;
+using to_do_list.web.ViewModels;
+
+namespace to_do_list.web.Controllers
+{
+    public class EditController : Controller
+    {
+        private readonly ToDoListDbContext _context;
+
+        public EditController(ToDoListDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int itemId)
+        {
+            var item = _context.ToDoItemModel.FindAsync(itemId);
+
+            var model = new ToDoListViewModel()
+            {
+                ToDoItem = new ToDoItemViewModel
+                {
+                    Id = item.Result.Id,
+                    Name = item.Result.Name,
+                    Completed = item.Result.Completed
+                }
+            };
+
+            return View("Edit", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(ToDoListViewModel editModel)
+        {
+
+            if (editModel.ToDoItem.Name == null || editModel.ToDoItem.Name == string.Empty)
+            {
+                return NotFound();
+            }
+
+            var model = new ToDoItemModel { Id = editModel.ToDoItem.Id, Name = editModel.ToDoItem.Name, Completed = editModel.ToDoItem.Completed };
+
+            _context.ToDoItemModel.Update(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
