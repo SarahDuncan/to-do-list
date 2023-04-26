6ecddc58c8c7d2896fe08606966c6d9b84c81953
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-04-26 19:13:40 +0100

    fix merge conflicts and add missing files

---
+++ to-do-list.web/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace to_do_list.web.ViewModels
+{
+    public class ToDoItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+}
